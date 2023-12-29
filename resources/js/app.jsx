506d4fb40f98667e16cc95f0339f02c62c1bc101
klismannecaj <102987499+weteak@users.noreply.github.com>
2023-12-30 00:41:56 +0100

    remow sw.js

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -19,12 +19,3 @@ createInertiaApp({
         color: '#549CD1',
     },
 });
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js') 
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  }
--- sw.js
+++
@@ -1,5 +0,0 @@
-// sw.js
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.1/workbox-sw.js');
-
-workbox.precaching.precacheAndRoute([]);
